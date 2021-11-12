c22a9728856fc8cbbdbe20ec93feeab7039cc853
lotusbleudesign <lotus.b78@gmail.com>
2021-11-12 22:22:08 +0100

    API Key consigne

--- app-sourcerer/src/environments/environment.ts
+++ app-sourcerer/src/environments/environment.ts
@@ -1,11 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-// 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
-// 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
+
 export const environment = {
   production: false,
-  api_key: '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',
+  api_key: 'your_key',
 
   uri: 'https://api.github.com/graphql' // <-- add the URL of the GraphQL server here};
 }
